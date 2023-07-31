40371d51cf34eed1fca43e885ce5787b651445a7
Ifeanyi Osuji <oindubuisi@gmail.com>
2023-07-31 04:11:32 +0100

    Added useEffect, useState, useMemo and Router

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,13 +1,35 @@
 import Header from './header';
-
+import { useEffect, useState, useMemo } from 'react'; // enables us to create side effects when the state of a component changes
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './main-page.css';
 
 function App() {
+  [allHouses, setAllHouses] = useState([])
+  useEffect(() => {
+    const fetchHouses = async ()=>{
+      const rsp = await fetch("/houses.json");
+      const houses = await rsp.json();
+      setAllHouses(houses);
+    };
+    fetchHouses();
+  }, []);
+  let featuredHouse = useMemo(() =>{
+    if(allHouses.length){
+      const randomIndex = Math.floor(Math.random()*allHouses.length);
+      return allHouses[randomIndex];
+  }}, [allHouses]);
+  
   return (
+    <Router>
     <div className='container'>
       <Header subtitle='Providing houses all over the world'/>
 
     </div>
+    <Switch>
+      <Route path='/'></Route>
+      </Switch>
+    </Router>
+  
    
   );
 }
