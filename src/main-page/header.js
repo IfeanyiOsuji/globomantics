95d53ccc766a4cb3465511cf0d7f1a3174823591
Ifeanyi Osuji <oindubuisi@gmail.com>
2023-07-31 03:25:55 +0100

    Added header component

---
+++ src/main-page/header.js
@@ -0,0 +1,18 @@
+import logo from './GloboLogo.png'
+
+import React from 'react'
+
+const Header = ({subtitle}) => {
+  return (
+    <header className='row'>
+    <div className='col-md-5'>
+        <img src={logo} className='logo' alt='logo'/>
+    </div>
+    <div className='col-md-5 mt-5 subtitle'>
+        {subtitle}
+    </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/main-page/index.js
@@ -0,0 +1,15 @@
+import Header from './header';
+
+import './main-page.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <Header subtitle='Providing houses all over the world'/>
+
+    </div>
+   
+  );
+}
+
+export default App;
